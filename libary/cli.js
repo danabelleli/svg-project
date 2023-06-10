1e04ad8261622fceef6b7c49d6cb29f9c09ead97
dannabelleli <belleli.danna@gmail.com>
2023-06-10 10:33:46 -0700

    added questions array and changed the inquirer version to the accurate one

--- libary/cli.js
+++ libary/cli.js
@@ -1,13 +1,44 @@
 const inquirer = require('inquirer');
+//import inquirer from 'inquirer';
 const { writeFile } = require('fs/promises');
 const SVG = require('./svg');
 const { Circle, Triangle, Square } = require('./shapes');
+
+const questions = [{
+    type: 'input',
+    message: "Write logo text (max 3 chars)",
+    name: 'logoText',
+},
+{
+    type: 'input',
+    message: 'Type logoText color',
+    name: 'logoTextColor',
+},
+{
+    type: 'list',
+    message: 'Select logo shape',
+    name: 'logoShape',
+    choices: [
+        'Circle',
+        'Triangle',
+        'Square'
+    ]
+},
+{
+    type: 'input',
+    message: 'Type logoShape color',
+    name: 'logoShapeColor',
+},
+];
 
 class CLI {
     run() {
         // return inquirer.prompt()
         // ask the user about shapeType, shapeColor, textColor and text
-
+        inquirer
+            .prompt(questions)
+            .then((answer) => console.log(answer.logoText, answer.logoTextColor, answer.logoShape, answer.logoShapeColor)
+            );
 
         // .then(answers)
         let shape;
